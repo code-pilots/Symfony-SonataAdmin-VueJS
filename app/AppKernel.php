974e2ba4a51c7c5d1e1be08d8fedfafb30bb50aa
oksana <oksana.iv85@gmail.com>
2017-11-07 16:40:46 +0300

    Upd conf

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -22,8 +22,8 @@ class AppKernel extends Kernel
             new Sonata\AdminBundle\SonataAdminBundle(),
             new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
 
-            new FOS\UserBundle\FOSUserBundle(),
-            new Sonata\UserBundle\SonataUserBundle('FOSUserBundle'),
+//            new FOS\UserBundle\FOSUserBundle(),
+//            new Sonata\UserBundle\SonataUserBundle('FOSUserBundle'),
 
             new AppBundle\AppBundle(),
         ];
