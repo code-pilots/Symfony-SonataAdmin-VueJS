9e9b248a93ecb4fc76d5552c9beb8021462d0208
Oksana <oksana.iv85@gmail.com>
2017-11-20 21:19:47 +0300

    Comment sonata dependencies

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,14 +16,11 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
 
-            new Sonata\CoreBundle\SonataCoreBundle(),
-            new Sonata\BlockBundle\SonataBlockBundle(),
-            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
-            new Sonata\AdminBundle\SonataAdminBundle(),
-            new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
-
-//            new FOS\UserBundle\FOSUserBundle(),
-//            new Sonata\UserBundle\SonataUserBundle('FOSUserBundle'),
+//            new Sonata\CoreBundle\SonataCoreBundle(),
+//            new Sonata\BlockBundle\SonataBlockBundle(),
+//            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+//            new Sonata\AdminBundle\SonataAdminBundle(),
+//            new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
 
             new AppBundle\AppBundle(),
         ];
